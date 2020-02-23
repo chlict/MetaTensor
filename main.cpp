f2185900ffa8cd5e0960dd2ccc61eff8adbfb77c
chenlong <chlict@163.com>
2020-02-23 15:36:14 +0800

    Add more tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-#include "calc.hpp"
-#include <boost/hana.hpp>
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    auto a = calc();
-    a.print();
-    auto b = boost::hana::make_tuple(1, 2);
     return 0;
 }
---
+++ test/TestDims.cpp
@@ -0,0 +1,40 @@
+#include "gtest/gtest.h"
+#include "Dims.hpp"
+#include "boost/hana.hpp"
+
+using namespace boost::hana::literals;
+
+TEST(TestDims, Test1) {
+    auto dims = Dims(1, 2, 3);
+    static_assert(dims.nDims == 3);
+    ASSERT_EQ(3, dims.nDims);
+}
+
+TEST(TestDims, Test2) {
+    auto dims = Dims(1, 2, 3);
+
+    ASSERT_EQ(dims.data[0_c], 1);
+    ASSERT_EQ(dims.data[1_c], 2);
+}
+
+TEST(TestDims, Test3) {
+    // Add constexpr
+    auto constexpr dims = Dims(1, 2, 3);
+
+    static_assert(dims.data[0_c] == 1);
+    static_assert(dims.data[1_c] == 2);
+
+    ASSERT_EQ(dims.data[0_c], 1);
+    ASSERT_EQ(dims.data[1_c], 2);
+}
+
+TEST(TestDims, Test4) {
+    // No constexpr
+    auto dims = Dims(1_c, 2_c, 3_c);
+
+    static_assert(dims.data[0_c] == 1_c);
+    static_assert(dims.data[1_c] == 2_c);
+
+    ASSERT_EQ(dims.data[0_c], 1);
+    ASSERT_EQ(dims.data[1_c], 2);
+}
