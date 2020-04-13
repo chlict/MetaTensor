40e34354260fb006a3fc20caa821893c728a9098
chenlong <chlict@163.com>
2020-04-13 22:56:11 +0800

    Update TilingProvider

--- src/includes/TilingProvider.hpp
+++ src/includes/TilingProvider.hpp
@@ -20,6 +20,7 @@ struct AbstractTilingProvider {
 };
 
 struct VectorTilingProvider : AbstractTilingProvider<VectorTilingProvider> {
+
     template <typename Tensor, typename Tiling>
     static constexpr auto gen_tiling_indices(Tensor const &tensor, Tiling const &tiling) {
         static_assert(is_tensor_type<Tensor> && is_a<ttiling_tag, Tiling>);
@@ -28,11 +29,17 @@ struct VectorTilingProvider : AbstractTilingProvider<VectorTilingProvider> {
         static_assert(is_dims_type<decltype(tensor_view)> && is_dims_type<decltype(dim_orders)>);
         static_assert(decltype(tensor_view)::nDims == 1 && decltype(dim_orders)::nDims == 1);
 
-        auto tiling_range = tiling.ranges()[0_c];
-        auto b = tiling_range.begin();
-        auto e = tiling_range.end();
-        int step = tiling_range.step();
-        auto indices = ranges::views::ints((int)b, (int)e) | ranges::views::filter([step](int x) { return x / step == 0; });
+        namespace views = ranges::views;
+        // Given a tiling range of [4, 10, 2], generates a sequence of [4, 6, 8, 10)
+        auto trange = tiling.ranges()[0_c];
+        auto b = trange.begin();
+        auto e = trange.end();
+        auto step = trange.step();
+        // ceil((e - b) / step)
+        int count = ((e - b) % step) == 0 ? (e - b) / step : (e - b) / step + 1;
+
+        int bi = (int)b;
+        auto indices = views::ints(0, count) | views::transform([bi, step](int x) { return bi + x * step; });
         return indices;
     }
 
--- test/TestTilingProvider.cpp
+++ test/TestTilingProvider.cpp
@@ -4,20 +4,32 @@
 #include "VectorFormat.hpp"
 #include "TilingProvider.hpp"
 
+// Zero-cost expected
 TEST(TestTilingProvider, Test1) {
-    auto format = make_format(Dim1(4_c), VectorLayout());
+    auto format = make_format(Dim1(10_c), VectorLayout());
     auto tensor = Tensor(float(), format, MemSpace::GM(), 0x1000);
-
-    auto tiling1d = Tiling1D(TRange(0_c, 4_c, 2_c, 1_c));
-    // std::cout << tiling1d << std::endl;
-
     auto provider = VectorTilingProvider();
-    auto indicies = provider.gen_tiling_indices(tensor, tiling1d);
-    for (auto i : indicies) {
-        printf("i = %d\n", i);
-    }
 
-    for (auto iter = indicies.begin(); iter != indicies.end(); iter++) {
-        printf("i = %d\n", *iter);
+    {
+        auto tiling1d = Tiling1D(TRange(0_c, 4_c, 2_c, 1_c));
+        auto indices = provider.gen_tiling_indices(tensor, tiling1d);
+        assert(indices[0] == 0 && indices[1] == 2);
     }
+    {
+        auto tiling1d = Tiling1D(TRange(2_c, 10_c, 2_c, 1_c));
+        auto indices = provider.gen_tiling_indices(tensor, tiling1d);
+        assert(indices.size() == 4 && indices[0] == 2 && indices[1] == 4 && indices[2] == 6 && indices[3] == 8);
+    }
+    {
+        auto tiling1d = Tiling1D(TRange(2_c, 10_c, 3_c, 1_c));
+        auto indices = provider.gen_tiling_indices(tensor, tiling1d);
+        assert(indices.size() == 3 && indices[0] == 2 && indices[1] == 5 && indices[2] == 8);
+    }
+//    for (auto i : indices) {
+//        printf("i = %d\n", i);
+//    }
+//
+//    for (auto iter = indices.begin(); iter != indices.end(); iter++) {
+//        printf("i = %d\n", *iter);
+//    }
 }
