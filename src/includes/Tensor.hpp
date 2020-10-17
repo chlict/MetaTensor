a5cb892f64732c4690da52574baed81d0ff8f32f
chenlong <chlict@163.com>
2020-10-17 21:47:01 +0800

    Refactoring

--- src/includes/Tensor.hpp
+++ src/includes/Tensor.hpp
@@ -30,13 +30,11 @@ template<typename ElemType, typename Format, typename Space, typename Addr>
 struct Tensor : TensorHandle {
     using tag = tensor_tag;
 
-    static_assert(is_a<mem_space_tag, Space> &&
-            is_a<tensor_format_tag, Format> &&
-            is_integral_or_constant<Addr>);
-
+private:
     const Format format_;
     const Addr addr_;
 
+public:
     constexpr Tensor(ElemType const &type, Format const &format, Space const &space, Addr const &addr) :
             format_(format), addr_(addr) {}
 
@@ -44,6 +42,7 @@ struct Tensor : TensorHandle {
 
     constexpr Tensor(Tensor &&other) noexcept : format_(std::move(other.format_)), addr_(std::move(other.addr_)) {}
 
+public:
     constexpr auto format() const { return format_; }
 
     constexpr auto addr() const { return addr_; }
@@ -58,32 +57,47 @@ struct Tensor : TensorHandle {
 
     template <typename Pos, typename TileShape>
     constexpr auto get_tile(Pos &&pos, TileShape &&tile_shape) const {
-        using shape_type = typename format_traits<Format>::shape_type;
-        static_assert(is_dims_type<std::remove_reference_t<Pos> > &&
-                      is_dims_type<std::remove_reference_t<TileShape> >);
-        static_assert(std::remove_reference_t<Pos>::nDims == shape_type::nDims &&
-                      std::remove_reference_t<TileShape>::nDims == shape_type::nDims);
-        // assert(pos within shape && tile_shape within shape);
+        get_tile_check<Format, Pos, TileShape>();
 
         // layout is same as original layout
-        using LayoutProvider = typename format_traits<Format>::layout_provider_type;
-        auto tile_format = make_format(std::forward<TileShape>(tile_shape), LayoutProvider());
-        auto offset = LayoutProvider::offset(std::forward<Pos>(pos), layout());
-        if constexpr (hana::is_a<hana::integral_constant_tag<long long>>(offset)
-            && hana::is_a<hana::integral_constant_tag<long long>, Addr>) {
-            auto tile_addr = addr() + offset * hana::llong_c<sizeof(ElemType)>;
-            return Tensor<ElemType, decltype(tile_format), Space, decltype(tile_addr)>(
-                    ElemType(), tile_format, Space(), tile_addr);
-        } else {
-            auto tile_addr = (uint64_t)addr() + sizeof(ElemType) * (uint64_t)offset;
-            return Tensor<ElemType, decltype(tile_format), Space, decltype(tile_addr)>(
-                    ElemType(), tile_format, Space(), tile_addr);
-        }
+        using LayoutType = typename format_traits<Format>::layout_provider_type;
+        auto tile_format = make_format(std::forward<TileShape>(tile_shape), LayoutType());
+        auto offset = LayoutType::offset(std::forward<Pos>(pos), layout());
+
+        using TileFormat = decltype(tile_format);
+        using TileOffset = decltype(offset);
+
+        constexpr bool is_static_tiling =
+            hana::is_a<hana::integral_constant_tag<long long>>(offset) &&
+            hana::is_a<hana::integral_constant_tag<long long>, Addr>;
+
+        auto tile_addr = hana::if_(
+            is_static_tiling,
+            addr() + offset * hana::llong_c<sizeof(ElemType)>,
+            (uint64_t)addr() + sizeof(ElemType) * (uint64_t)offset);
+
+        return Tensor<ElemType, TileFormat, Space, decltype(tile_addr)>(
+            ElemType(), tile_format, Space(), tile_addr);
     }
 
     friend std::ostream& operator << (std::ostream &os, Tensor tensor) {
         os << "Tensor@0x" << std::hex << tensor.addr() << std::dec;
         return os;
+    }
+
+private:
+    static_assert(is_a<mem_space_tag, Space> &&
+                  is_a<tensor_format_tag, Format> &&
+                  is_integral_or_constant<Addr>);
+
+    template <typename TensorFormat, typename Pos, typename TileShape>
+    constexpr void get_tile_check() const {
+        using shape_type = typename format_traits<TensorFormat>::shape_type;
+        static_assert(is_dims_type<std::remove_reference_t<Pos> > &&
+                      is_dims_type<std::remove_reference_t<TileShape> >);
+        static_assert(std::remove_reference_t<Pos>::nDims == shape_type::nDims &&
+                      std::remove_reference_t<TileShape>::nDims == shape_type::nDims);
+        // assert(pos within shape && tile_shape within shape);
     }
 };
 
